86f564840f2260d7e6b780c25feedebe2a70e42f
mihushynmaksym <mihushynmaksym@gmail.com>
2017-02-19 21:53:27 +0200

    Create packages for different responsibility zones

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
-from group import Group
-from application import Application
 import pytest
+
+from fixture.application import Application
+from model.group import Group
+
 
 @pytest.fixture
 def app(request):
