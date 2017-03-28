d7054fbec0dbef8b30bba2723d21eac24a76c656
mihushynmaksym <mihushynmaksym@gmail.com>
2017-03-29 00:14:50 +0300

    Add representation and equals

--- test/test_delete_group.py
+++ test/test_delete_group.py
@@ -12,3 +12,6 @@ def test_delete_first_group(app):
     app.group.delete_first_group()
     new_group = app.group.get_group_list()
     assert len(old_groups) - 1 == len(new_group)
+    old_groups[0:1] = []
+    assert old_groups == new_group
+
