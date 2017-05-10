d9719522c0990a1cf0b134ab2568bd0a9d079d0d
mihushynmaksym <mihushynmaksym@gmail.com>
2017-05-10 23:13:05 +0300

    Add JSON Config_file
    1)Adds JSON config file
    2)Changed default directory for compiler
    3)Create global "target"

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 
 import pytest
+import json
 from selenium import webdriver
 from fixture.sesion import SessionHelper
 from fixture.group import GroupHelper
@@ -8,19 +9,20 @@ __author__ = 'Max'
 
 
 fixture = None
+target = None
 
 
 @pytest.fixture
 def app(request):
     global fixture
+    global target
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--baseUrl")
-    if fixture is None:
-        fixture = Application(browser,base_url)
-    else:
-        if not fixture.is_valid:
-            fixture = Application(browser,base_url)
-    fixture.session.ensure_login(username="admin", password="secret")
+    if target is None:
+        with open(request.config.getoption("--target")) as config_file:
+            target = json.load(config_file)
+    if fixture is None or fixture.is_valid():
+        fixture = Application(browser,base_url=target["baseUrl"])
+    fixture.session.ensure_login(username=target["username"], password=target["password"])
     return fixture
 
 
@@ -37,7 +39,7 @@ def pytest_addoption(parser):
     # hooks for browsers
     parser.addoption("--browser",action="store",default="chrome")
     # hooks for links
-    parser.addoption("--baseUrl",action="store",default="http://localhost/addressbook/group.php")
+    parser.addoption("--target",action="store",default="target.json")
 
 
 class Application:
